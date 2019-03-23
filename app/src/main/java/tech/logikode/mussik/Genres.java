ee8e9f83c3a3d697ad6e0a2bbf28eb58376ef034
Jose Aquino <joseaquino96@gmail.com>
2019-03-23 17:28:24 -0600

    Added rock & roll genre assets

--- app/src/main/java/tech/logikode/mussik/Genres.java
+++ app/src/main/java/tech/logikode/mussik/Genres.java
@@ -7,7 +7,7 @@ class Genres {
 
     Genres() {
         genres.add(new Genre("Jazz", R.drawable.jazz_cover));
-        genres.add(new Genre("Jazz", R.drawable.jazz_cover));
+        genres.add(new Genre("Rock", R.drawable.rock_and_roll_cover));
         genres.add(new Genre("Jazz", R.drawable.jazz_cover));
         genres.add(new Genre("Jazz", R.drawable.jazz_cover));
     }
